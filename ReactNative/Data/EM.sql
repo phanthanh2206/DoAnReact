b25b33eff2313a7d004aeaab9898f345c3857039
phanthanh2206 <thanhpro2206@gmail.com>
2023-09-25 20:03:18 +0700

    Update DB

---
+++ ReactNative/Data/EM.sql
@@ -0,0 +1,106 @@
+CREATE DATABASE `employee_management_1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+use `employee_management_1`;
+CREATE TABLE `account` (
+   `account_id` int(11) NOT NULL AUTO_INCREMENT,
+   username varchar(20) NOT NULL,
+   `password` varchar(20) NOT NULL,
+   email varchar(20) NOT NULL,
+   is_activaed bit DEFAULT 0,
+PRIMARY KEY (`account_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE account_detail (
+  `account_id` int(11) NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL,
+  `gender` int(11) NOT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `skill` varchar(200) DEFAULT NULL,
+  `level` varchar(20) DEFAULT NULL,
+  `department_id` int(11) NOT NULL,
+  `position_id` int(11) NOT NULL,
+  `created_dated` date DEFAULT NULL,
+  `updated_date` date DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`account_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `role` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `bonus` (
+  `bonus_id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `reason` varchar(200) DEFAULT NULL,
+  `created_dated` date DEFAULT NULL,
+  `updated_date` date DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`bonus_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `contract` (
+  `contract_id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `duration` varchar(20) NOT NULL,
+  `pay_roll` float NOT NULL,
+  `annual` float NOT NULL,
+  `created_dated` date DEFAULT NULL,
+  `updated_date` date DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`contract_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `department` (
+  `department_id` int(11) NOT NULL AUTO_INCREMENT,
+  `department_name` varchar(50) NOT NULL,
+  `description` varchar(400) DEFAULT NULL,
+  `created_dated` date DEFAULT NULL,
+  `updated_date` date DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`department_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `leave` (
+  `leave_id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `reason` varchar(200) DEFAULT NULL,
+  `created_dated` date DEFAULT NULL,
+  `updated_date` date DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`leave_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `position` (
+  `position_id` int(11) NOT NULL AUTO_INCREMENT,
+  `position_name` varchar(50) NOT NULL,
+  `description` varchar(400) DEFAULT NULL,
+  `created_dated` date DEFAULT NULL,
+  `updated_date` date DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`position_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+CREATE TABLE `salary` (
+  `salary_id` int(11) NOT NULL AUTO_INCREMENT,
+  `actual_salary` float NOT NULL,
+  `pay_date` date DEFAULT NULL,
+  `leave_id` int(11) NOT NULL,
+  `bonus_id` int(11) NOT NULL,
+  `created_dated` date DEFAULT NULL,
+  `updated_date` date DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`salary_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
